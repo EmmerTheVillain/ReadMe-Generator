06610afa919dcf51d8e219037ae5f71d5cdffed3
Mike Barber <bandofgold1@gmail.com>
2023-07-26 16:20:58 -0400

    updated index
    added questions

--- index.js
+++ index.js
@@ -1,11 +1,57 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    prompt([
+        {
+          type: 'input',
+          message: 'What is your README file name?',
+          name: 'fileName',
+        },
+        {
+          type: 'input',
+          message: 'What is your project title?',
+          name: 'projectTitle',
+        },
+        {
+          type: 'input',
+          message: 'Please provide a short description:',
+          name: 'description',
+        },
+        {
+            type: 'input',
+            message: 'Please provide installation instructions:',
+            name: 'installInstructions',
+        },
+        {
+            type: 'input',
+            message: 'Please describe the usage of this application:',
+            name: 'usage',
+        },
+        {
+            type: 'input',
+            message: 'Please provide a short description:',
+            name: 'description',
+          },
+      ])
+      .then((response) =>
+        response.confirm === response.password
+          ? console.log('Success!')
+          : console.log('You forgot your password already?!')
+      );
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile('/', content, err => {
+        if (err) {
+          console.error(err);
+        }
+        // file written successfully
+      });
+}
 
 // TODO: Create a function to initialize app
 function init() {}
